ee078fc8af423320956bbeed419196d8fb00226a
Mirla Isabel <mirlaisabel11@gmail.com>
2022-07-31 21:58:40 -0300

    urls changes

--- curriculo/urls.py
+++ curriculo/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls') )
+    path('', views.index, name='index')
 ]
